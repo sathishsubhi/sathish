7d9586302263b884a763ba5c979670cb64f83995
sathishsubhi <sathishvijay79@gmail.com>
2021-10-23 18:08:08 +0530

    limit

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,23 @@
+package org.company;
+
+public class CompanyInfo {
+	public void companyName() {
+		System.out.println("SL Lumax");
+		
+	}
+	public void companyId() {
+		System.out.println("TN01456");
+	}
+	public void companyAdress() {
+		System.out.println("tambaram,chennai");
+	}
+	public static void main(String[] args) {
+		CompanyInfo c=new CompanyInfo();
+		c.companyName();
+		c.companyId();
+		c.companyAdress();
+		
+	}
+	
+
+}
